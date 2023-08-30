64e0ba40b9eb357e7c54bb0d9578c850958da7ff
Kash Arcot <arcotk@google.com>
2023-08-30 10:49:48 -0500

    sdkfjgsdf

--- quickstart.sh
+++ quickstart.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
+
 echo "GCP Cloud Build started at $(date)."
 
 airflow db init
 
 airflow dags list-import-errors --subdir ./dags/ >> import_errors.txt
 
-if [-s import_errors.txt]; then
-    echo "Error! DAGs have syntax errors. Please validate"
-    echo import_errors.txt
-    # exit 1
-else
-    echo "No syntax errors found."
+if [ -f import_errors.txt ]; then
+    if [ -s import_errors.txt ] then
+        echo "Error! DAGs have syntax errors. Please validate"
+        echo import_errors.txt
+        #exit 1
+    else
+        echo "No syntax errors found."
+    fi
 fi
 
 echo "Validation ended at $(date)."
