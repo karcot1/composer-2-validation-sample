8afec61c89f79f8a51cfa69c3663f4f2a80f44c5
Kash Arcot <arcotk@google.com>
2023-09-06 11:00:43 -0500

    revising if statement

--- quickstart.sh
+++ quickstart.sh
@@ -6,13 +6,13 @@ airflow db init
 
 airflow dags list-import-errors --subdir ./dags/ >> import_errors.txt
 
-if [ -s import_errors.txt ] 
+if grep -q "No data found" import_errors.txt
 then
+    echo "No syntax errors found."
+else
     echo "Error! DAGs have syntax errors. Please validate"
     cat import_errors.txt
     exit 1
-else
-    echo "No syntax errors found."
 fi
 
 echo "Validation ended at $(date)."
